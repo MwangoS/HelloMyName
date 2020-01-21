fb4c4b262c7cdce2b58fdba056b905da005f3aea
MwangoS <natchaziya@gmail.com>
2020-01-21 14:21:44 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloName
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Mwango!");
+        }
+    }
+}
